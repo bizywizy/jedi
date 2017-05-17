a3fb185f82bb985df8330c3b26c5c56411d0758e
bizywizy <bizywiz@gmail.com>
2017-05-17 19:04:48 +0300

    added models, urls refer to jedi app, base template and index page

--- project/jedi/models.py
+++ project/jedi/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Jedi(models.Model):
+    name = models.CharField(max_length=100)
+    planet = models.ForeignKey(Planet)
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    planet = models.ForeignKey(Planet)
+    age = models.IntegerField()
+    email = models.EmailField()
+    jedi = models.ForeignKey(Jedi, null=True)
+
+
+class Question(models.Model):
+    text = models.TextField()
+    right_answer = models.BooleanField()
+
+
+class Answer(models.Model):
+    candidate = models.ForeignKey(Candidate)
+    question = models.ForeignKey(Question)
+    answer = models.BooleanField()
+
+
+class Challenge(models.Model):
+    # решил, что на каждой планете по одному ордену, дабы не вводить еще одну сущность
+    order = models.ForeignKey(Planet)
+    question = models.ManyToManyField(Question)
--- project/project/settings.py
+++ project/project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -27,10 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'jedi',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,17 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'bizy',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -99,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -112,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
