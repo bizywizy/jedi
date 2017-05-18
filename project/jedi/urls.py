bf9394c48cabfe6a231713c884a43a3f37166347
bizywizy <bizywiz@gmail.com>
2017-05-18 18:25:01 +0300

    added new candidate view and urls

--- project/jedi/urls.py
+++ project/jedi/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from . import views
+
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='jedi/index.html')),
+    url(r'candidate$', views.NewCandidateView.as_view(), name='new-candidate'),
+    url(r'candidate/(?P<candidate_id>[0-9]+)/order/(?P<order_id>[0-9]+)/try$',
+        views.ChallengeView, name="try-challenge"),
+
 ]
--- project/jedi/views.py
+++ project/jedi/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.edit import CreateView, FormView
+from .forms import CandidateForm
+
+
+class NewCandidateView(CreateView):
+    form_class = CandidateForm
+    template_name = 'jedi/new_candidate.html'
+
+    def get_success_url(self):
+        return reverse('try-challenge', kwargs={'candidate_id': self.object.pk,
+                                                'order_id': self.object.planet_id})
+
+
+class ChallengeView(FormView):
+    pass
